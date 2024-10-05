6be4b7a1abc192d9f8c26049deea8ab670ce5c90
igna_q <j.ignacioquintero@gmail.com>
2024-10-05 01:25:25 -0300

    metodo add productos

--- src/services/ProductManager.js
+++ src/services/ProductManager.js
@@ -20,12 +20,37 @@ export default class ProductManager{
             this.products = []
         }
     }
+
+    saveToFile() {
+        fs.writeFile(filePath, JSON.stringify(this.products, null, 2))
+    }
+
   
     getAll(limit){
         if(limit){
             return this.products.slice(0, limit)
         }
         return this.products;
+    }
+
+    add(product) {
+        const newProduct = {
+            id: this.products.length ? this.products[this.products.length - 1].id + 1 : 1,
+            name: product.name,  
+            price: product.price,
+            description: product.description, 
+            status: product.status,
+            title: product.title,
+            description: product.description,
+            code: product.code,
+            price: product.price,
+            stock: product.stock,
+            category: product.category,
+            thumbnails: product.thumbnails,
+        };
+        this.products.push(newProduct);
+        this.saveToFile();
+        return newProduct;
     }
 
 }
