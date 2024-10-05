294df4d8ac02ecc711c305a1eefa718d88f9411f
igna_q <j.ignacioquintero@gmail.com>
2024-10-04 22:41:19 -0300

    se importa manager de productos en router de productos

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,9 +1,22 @@
 import { Router } from 'express'
+import ProductManager from '../services/ProductManager.js';
+
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.send('test')
+const productManager = new ProductManager()
+
+
+router.get('/', async (req, res) => {
+
+    try {
+        const limit = req.query.limit ? parseInt(req.query.limit) : undefined;
+        const products = await productManager.getAll(limit);
+        res.json(products);
+    } catch (error) {
+        console.log(error)
+    }
 })
+
 
 export default router;
