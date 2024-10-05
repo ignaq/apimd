8e7a03a33e7f6a48d05b74c8e75aa91a45cc1a2d
igna_q <j.ignacioquintero@gmail.com>
2024-10-05 01:24:04 -0300

    router creación productos

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import ProductManager from '../services/ProductManager.js';
-
+import { check } from '../utils/checkProducts.js';
 
 const router = Router()
 
@@ -15,6 +15,25 @@ router.get('/', async (req, res) => {
         res.json(products);
     } catch (error) {
         console.log(error)
+    }
+})
+
+
+
+router.post('/', async (req, res) => {
+    try {
+        const { title, description, code, price, stock, category,  thumbnails = [], status = true } = req.body;
+        const errors = check({ title, description, code, price, stock, category });
+
+        if (errors.length > 0) {
+            return res.status(400).json({ errors });
+        }
+      
+        const product = await productManager.add({ title, description, code, price, stock, category, thumbnails, status })
+        res.status(201).json(product)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error en la creación del producto' });
     }
 })
 
