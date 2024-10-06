784d9e58d92795070aeec1fb86c81d603efe43e3
igna_q <j.ignacioquintero@gmail.com>
2024-10-06 17:33:03 -0300

    se agrega router para agregar productos en carrito

--- src/routes/carts.js
+++ src/routes/carts.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import CartManager from '../services/CartManager.js'
-
+import ProductManager from '../services/ProductManager.js'
+import { check } from '../utils/checkProdAndCart.js'
 
 const router = Router()
 
 const cartManager = new CartManager()
+const productManager = new ProductManager();
 
 router.post('/', async (req, res) => {
     try {
@@ -33,5 +35,30 @@ router.get('/:cid', async (req, res) => {
     }
 })
 
+
+router.post('/:cid/product/:pid', async (req, res) => {
+    try {
+        const cartId = parseInt(req.params.cid)
+        const productId = parseInt(req.params.pid)
+
+        const cart = await cartManager.getBydId(cartId)
+        const product = await productManager.getBydId(productId)
+
+        if(cart && product){
+            const result = await cartManager.addProduct(cart, product);
+            if (result) {
+            res.json(result);
+            } else {
+                res.status(404).json({ error: 'No se puede agregar un producto en el carrito' });
+            }
+        }else{
+            const errors = check({ cart, product});
+            return res.status(400).json({ errors });
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 export default router;
