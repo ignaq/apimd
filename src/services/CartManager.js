15eaaeb0530e407b07b0fec8bdda38d28fb4c553
igna_q <j.ignacioquintero@gmail.com>
2024-10-06 17:30:26 -0300

    se agrega metodo para agregar productos al carrito

--- src/services/CartManager.js
+++ src/services/CartManager.js
@@ -35,6 +35,30 @@ export default class CartManager{
         return newCart;
     }
 
+    addProduct(updateCart, product) {
+        const cartIndex = this.carts.findIndex(cart => cart.id === updateCart.id);
+        if (cartIndex === -1) return null;
+            if(updateCart.products.length === 0){
+                this.carts[cartIndex] = { id: this.carts[cartIndex].id, products: [{ id: product.id, quantity: 1 }]};
+                this.saveToFile();
+                return this.carts[cartIndex];
+            }else{
+                //busco si existe el producto en el carrito
+                const productIndex = updateCart.products.findIndex(prod => prod.id === product.id);
+                if(productIndex === -1){
+                    this.carts[cartIndex].products.push({ id: product.id, quantity: 1 }) 
+                    this.saveToFile();
+                    return this.carts[cartIndex]
+                }else{
+                    this.carts[cartIndex].products[productIndex].quantity += 1;
+                    this.saveToFile();
+                    return this.carts[cartIndex]
+                    // console.log("El producto ya existe. Hay que sumar la cantidad")
+                }
+               
+            }
+    }
+
     getBydId(id){
         return this.carts.find(carts => carts.id === id)
     }
