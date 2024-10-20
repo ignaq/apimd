f7b4f1fd17db39d45585b0649ab97576dbdb11aa
ignaq <j.ignacioquintero@gmail.com>
2024-10-20 18:57:05 -0300

    se modifica app para handlebars

--- src/app.js
+++ src/app.js
@@ -1,18 +1,44 @@
 import express from 'express'
+import handlebars  from 'express-handlebars';
+import __dirnameView from './utils/dir-util.js'
+import cors from 'cors'
+
+
 import products from './routes/products.js'
 import carts from './routes/carts.js'
+import routerView from './routes/views.js'
+
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors())
+
+const server = createServer(app); 
+
+const io = new Server(server);
+//handlebars
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirnameView);
+app.set('view engine', 'handlebars');
 
 
-//routes
+
+  // Rutas
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+});
+app.use(express.static(__dirnameView + "/public"))
 app.use("/api/products", products)
 app.use("/api/carts", carts)
 
+app.use('/', routerView)
+
 const PORT = 8080;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`)
 })
